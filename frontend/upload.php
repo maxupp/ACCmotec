d6c283b4326b366c92ae9f8a89b560aa4234e6d0
Max Uppenkamp <max.uppenkamp@rwth-aachen.de>
2021-03-14 23:50:27 +0100

    fixed uplaod dir

--- frontend/upload.php
+++ frontend/upload.php
@@ -1,5 +1,5 @@
 <?php
-$target_dir = "/motec_data/";
+$target_dir = "/var/www/html/motec_data/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $fileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
