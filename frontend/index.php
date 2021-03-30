cd6c7980d622677670e464cbd4e53afc4e248d49
Peter Nicholl <p.nicholl@ulster.ac.uk>
2021-03-30 13:07:03 +0100

    Removed unnecessary font awsome link

--- frontend/index.php
+++ frontend/index.php
@@ -12,7 +12,6 @@
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.24/css/jquery.dataTables.min.css" >
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css" >
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" >
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.24/css/dataTables.bootstrap4.min.css" >
 </head>
 <body>
